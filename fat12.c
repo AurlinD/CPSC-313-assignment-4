c87c6e0049eecdd1c4e87a53ee36acddbf54840d
Aurlin Dhillon <aurlin@dhcp-128-189-198-216.ubcsecure.wireless.ubc.ca>
2018-11-28 19:37:59 -0800

    working on read_file

--- fat12.c
+++ fat12.c
@@ -54,9 +54,9 @@ fat12volume *open_volume_file(const char *filename) {
 
     //setbuffer(fatd, buff, sizeof(struct fat12volume));
     fseek(f,0,SEEK_END);
-    size = ftell(f);
+    int size = ftell(f);
     fseek(f,0,SEEK_SET);
-    char * buff = malloc size;
+    char * buff = malloc(size);
     setbuffer(fatd, buff,size);
 
 
