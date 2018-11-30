f5b5587cbea63ddeee56f338655159a56b155160
Steven Gao <e1d0b@lin03.ugrad.cs.ubc.ca>
2018-11-29 18:07:25 -0800

    fade me

--- fat12.c
+++ fat12.c
@@ -44,14 +44,17 @@ unsigned int read_unsigned_le(const char *buffer, int position, int num_bytes) {
      smaller than necessary.
  */
 fat12volume *open_volume_file(const char *filename) {
-  FILE * fatd;
-  char buff = malloc(sizeof(struct fat12volume));
-  fatd = fopen(filename,"r");
+  FILE * fatd = fopen(filename,"r");
+  char* buff = (char*) malloc(BOOT_SECTOR_SIZE);
   struct fat12volume *fat = malloc(sizeof(struct fat12volume));
+  fread(buff, BOOT_SECTOR_SIZE, 1, fatd);
+  if (buff != NULL) {
+    fprintf(stderr, buff);
+  }
   if (*filename != NULL){
 
-    char* buff = (char*) malloc(BOOT_SECTOR_SIZE);
-    //setbuff(*filename, buff);
+    // char* buff = (char*) malloc(BOOT_SECTOR_SIZE);
+    // setbuff(*filename, buff);
     // fseek(fatd,0,SEEK_END);
     // int size = ftell(fatd);
     // fseek(fatd,0,SEEK_SET);
@@ -59,42 +62,42 @@ fat12volume *open_volume_file(const char *filename) {
     // setbuffer(fatd, buff,size);
 
     
-    if (fat->sector_size = read_unsigned_le(buff, 11, 1) == 0){
+    if ((fat->sector_size = read_unsigned_le(buff, 11, 1)) == 0){
         fprintf(stderr, "File is NULL 1\n");
         return fat; 
     }
 
-    if(fat->cluster_size = read_unsigned_le(buff, 13, 1) == 0){
+    if((fat->cluster_size = read_unsigned_le(buff, 13, 1)) == 0){
         fprintf(stderr, "File is NULL 2\n");
         return fat;
     }
 
-    if(fat->reserved_sectors = read_unsigned_le(buff, 14, 1) == 0){
+    if((fat->reserved_sectors = read_unsigned_le(buff, 14, 1)) == 0){
         fprintf(stderr, "File is NULL 3\n");
         return fat;
     }
 
-    if(fat->hidden_sectors = read_unsigned_le(buff, 28, 2) == 0){
+    if((fat->hidden_sectors = read_unsigned_le(buff, 28, 2)) == 0){
         fprintf(stderr, "File is NULL 4\n");
         return fat;
     }
 
-    if(fat->fat_num_sectors = read_unsigned_le(buff, 22, 2) == 0){
+    if((fat->fat_num_sectors = read_unsigned_le(buff, 22, 2)) == 0){
         fprintf(stderr, "File is NULL 5\n");
         return fat;
     };
 
-    if(fat->fat_copies = read_unsigned_le(buff, 16 , 1) == 0){
+    if((fat->fat_copies = read_unsigned_le(buff, 16 , 1)) == 0){
         fprintf(stderr, "File is NULL 6\n");
         return fat;
     };
 
-    if((fat->fat_offset = fat->reserved_sectors + fat->fat_num_sectors * fat->fat_copies) == 0){
+    if((fat->fat_offset = fat->reserved_sectors + fat->fat_num_sectors * fat->fat_copies)) == 0){
         fprintf(stderr, "File is NULL 7\n");
         return fat;
     }
 
-    if(fat->fat_array = read_unsigned_le(buff, 0, 512) == 0){
+    if((fat->fat_array = read_unsigned_le(buff, 0, 512)) == 0){
         fprintf(stderr, "File is NULL 8\n");
         return fat;
     }
@@ -108,12 +111,12 @@ fat12volume *open_volume_file(const char *filename) {
         return fat;
     }
 
-    if(fat->rootdir_array = read_unsigned_le(buff, 0 , 32) == 0){
+    if((fat->rootdir_array = read_unsigned_le(buff, 0 , 32)) == 0){
         fprintf(stderr, "File is NULL 11\n");
         return fat;
     }
 
-    if(fat->rootdir_num_sectors  = (fat->rootdir_entries / fat->sector_size) == 0){
+    if((fat->rootdir_num_sectors  = (fat->rootdir_entries / fat->sector_size)) == 0){
         fprintf(stderr, "File is NULL 12\n");
         return fat;
     }
