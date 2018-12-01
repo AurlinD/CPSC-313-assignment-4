55c4397cc1f29f50da5e35c40f61ee95b36d0c18
stevengao <sgao1996@gmail.com>
2018-11-30 16:54:10 -0800

    blah

--- fat12.c
+++ fat12.c
@@ -76,7 +76,7 @@ fat12volume *open_volume_file(const char *filename) {
 
     fat->fat_offset = fat->reserved_sectors + fat->fat_num_sectors * fat->fat_copies;
 
-    fat->fat_array = read_sectors(fat->volume_file, 1 , fat->fat_num_sectors, buff);
+    fat->fat_array = read_sectors(fat->volume_file, 1, fat->fat_num_sectors, buff);
 
     fat->rootdir_offset = fat->reserved_sectors + fat->rootdir_num_sectors * fat->fat_copies;
 
